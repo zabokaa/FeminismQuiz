804a4ee446a79f4da4b798d1c4602139b18f1ea1
zabokaa <sab_fem@gmx.de>
2024-01-22 22:48:38 +0100

    create questions data base

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,93 @@
-console.log('oi!');
+// data set of questions+answers
+let questions = [
+    // question1
+    {
+        question: "When and where was the word 'feminism' first used?",
+        answers: [
+            { text: "1882 in France", correct: true },
+            { text: "1894 in Great Britain", correct: false },
+            { text: "1968 in Germany", correct: false },
+        ]
+    },
+    // question2
+    {
+        question: "In the Weimar Republic, female civil servants were dismissed if they ...",
+        answers: [
+            { text: "had a child", correct: false},
+            { text: "were communists", correct: false },
+            { text: "no women at all", correct: true },
+        ]
+    },
+    // question3
+    {
+        question: "In 1919, the German National Assembly elected ...",
+        answers: [
+            { text: "10 per cent of female voters", correct: false },
+            { text: "90 per cent of female voters", correct: true },
+            { text: "women were not allowed to vote", correct: false },
+        ]
+    },
+    // question4
+    {
+        question: "What happened at the Battle of Women in Leipzig in 1865?",
+        answers: [
+            { text: "Women's associations argued about the future constitution", correct: false },
+            { text: "The first women's organisation in Germany was founded", correct: true },
+            { text: "Demonstrating women threw bread and vegetables ", correct: false },
+        ]
+    },
+    // question5
+    {
+        question: "In 1971, 374 women admitted ...",
+        answers: [
+            { text: "living with a woman", correct: false },
+            { text: "taking the contraceptive pill", correct: false },
+            { text: "have had an abortion", correct: true},
+        ]
+    },
+    // question6
+    {
+        question: "Since when have women been allowed to study at universities in Germany?",
+        answers: [
+            { text: "Since the Enlightenment in the 18th century", correct: false },
+            { text: "Since the foundation of the German Empire under Bismarck in 1871", correct: false },
+            { text: "Since the beginning of the Weimar Republic in 1918", correct: true },
+        ]
+    },
+    // question7
+    {
+        question: "What was a husband allowed to do in Germany until 1958?",
+        answers: [
+            { text: "Terminate the wife's employment contract", correct: true },
+            { text: "Force his wife to bear at least two children", correct: false },
+            { text: "Forbid his wife to go to the polls", correct: false },
+        ]
+    },
+    // question8
+    {
+        question: "In which country have women only been allowed to vote since 1984?",
+        answers: [
+            { text: "Switzerland", correct: false },
+            { text: "Liechtenstein", correct: true },
+            { text: "Saudi Arabia", correct: false },
+        ]
+    },
+    // question9
+    {
+        question: "What is the most common cause of death worldwide for women between the ages of 16 and 44? ",
+        answers: [
+            { text: "Feminicide", correct: true },
+            { text: "Cancer", correct: false },
+            { text: "Road traffic accidents", correct: false },
+        ]
+    },
+     // question10
+     {
+        question: "Until when was rape by the husband legal in Germany?",
+        answers: [
+            { text: "1968", correct: false },
+            { text: "1997", correct: true },
+            { text: "1952", correct: false },     
+        ]
+    }
+]
