82b926c2647cc3b109401a66a6ad84ff23a65e36
zabokaa <sab_fem@gmx.de>
2024-01-25 21:01:01 +0100

    add modal into js

--- assets/js/script.js
+++ assets/js/script.js
@@ -113,7 +113,32 @@ document.addEventListener("DOMContentLoaded", function() {
     const introContainer = document.getElementById("intro");
     const nextButton = document.getElementById("next-question-btn");
     const startButton = document.getElementById("start-quiz-btn");
+//////////
+
     const modalButton = document.getElementById("modal-btn");
+    const modal = document.getElementById("myModal");
+    const modalText = document.getElementById("modal-text")
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    modalButton.onclick = function() {
+    modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+    modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+    }
+
+///////////////////////////
     let randomQuestions = []; 
     let currentQuestion;
     let questionsRoundIndex = 0;
@@ -167,6 +192,8 @@ document.addEventListener("DOMContentLoaded", function() {
            }
            antwort.addEventListener("click", userAnswer)
     });
+    // here modal-text
+        modalText.innerHTML = currentQuestion.explanation;
 }
 
     function userAnswer(a) {
