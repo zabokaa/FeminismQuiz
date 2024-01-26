a99fef643139f354ac6b8d0a177597f30eb378e1
zabokaa <sab_fem@gmx.de>
2024-01-26 23:37:29 +0100

    create content for explanation in dataset.js

--- assets/js/dataset.js
+++ assets/js/dataset.js
@@ -7,7 +7,7 @@ let questions = [
             { text: "1894 in Great Britain", correct: false },
             { text: "1968 in Germany", correct: false },
         ],
-        explanation: "france"
+        explanation: "The French women's rights activist Hubertine Auclert first publicly called herself a féministe in 1882. The term féminisme is also documented from this time. The word first appeared in Great Britain in 1894."
     },
     // question2
     {
@@ -17,7 +17,7 @@ let questions = [
             { text: "were communists", correct: false },
             { text: "got married", correct: true },
         ],
-        explanation: "married"
+        explanation: "Despite several attempts, female parliamentarians in the Weimar Republic did not succeed in overturning the so-called celibacy of female civil servants. This meant that female civil servants were forced to resign from the civil service if they got married."
     },
     // question3
     {
@@ -27,7 +27,7 @@ let questions = [
             { text: "90 per cent of female voters", correct: true },
             { text: "women were not allowed to vote", correct: false },
         ],
-        explanation: "Lorem."
+        explanation: "During the November Revolution of 1918, the Council of People's Representatives introduced the right to vote and stand for election for all citizens aged 21 and over, including women. In the 1919 election to the National Assembly, almost 90 percent of eligible women cast their vote."
     },
     // question4
     {
@@ -37,7 +37,7 @@ let questions = [
             { text: "The first women's organisation in Germany was founded", correct: true },
             { text: "Demonstrating women threw bread and vegetables ", correct: false },
         ],
-        explanation: "yes."
+        explanation: "In mid-October 1865, the first supra-regional women's organization, the General German Women's Association, was founded in Leipzig. The press mocked the meeting as the 'Battle of the Women' because it took place at the same time as the anniversary of the Battle of the Nations."
     },
     // question5
     {
@@ -47,7 +47,7 @@ let questions = [
             { text: "taking the contraceptive pill", correct: false },
             { text: "have had an abortion", correct: true},
         ],
-        explanation: "abortion"
+        explanation: "With the development of a new left-wing movement in the Federal Republic of Germany, the women's movement also regrouped at the end of the 1960s. Central demand: the deletion of the 'abortion paragraph' 218 in the penal code. The high point of the campaign was the 'I had an abortion' campaign initiated by the journalist Alice Schwarzer, in which 374 women confessed to having an illegal abortion in magazine 'Stern' on June 6, 1971."
     },
     // question6
     {
@@ -57,7 +57,7 @@ let questions = [
             { text: "Since the foundation of the German Empire under Bismarck in 1871", correct: false },
             { text: "Since the beginning of the Weimar Republic in 1918", correct: true },
         ],
-        explanation: "1918."
+        explanation: "Women in Germany gained the right to study at universities in 1918, marking a significant milestone in gender equality. Previously, they could only study in Heidelberg (since 1900) or other countries. "
     },
     // question7
     {
@@ -67,7 +67,7 @@ let questions = [
             { text: "Force his wife to bear at least two children", correct: false },
             { text: "Forbid his wife to go to the polls", correct: false },
         ],
-        explanation: "work contractão."
+        explanation: "It's hard to believe, but the German Civil Code (BGB) stipulated that wives who wanted to work needed their husband's consent. This section of the BGB was not changed until 1977."
     },
     // question8
     {
@@ -77,7 +77,7 @@ let questions = [
             { text: "Liechtenstein", correct: true },
             { text: "Saudi Arabia", correct: false },
         ],
-        explanation: "Liechtião."
+        explanation: "Women's suffrage in Liechtenstein was introduced on 1 July 1984, after the 1984 Liechtenstein women's suffrage referendum. This was the last nation in Europe to introduce this right."
     },
     // question9
     {
@@ -87,7 +87,7 @@ let questions = [
             { text: "Cancer", correct: false },
             { text: "Road traffic accidents", correct: false },
         ],
-        explanation: "feminic"
+        explanation: "In 2022, around 48,800 women and girls worldwide were killed by their intimate partners or other family members (including fathers, mothers, uncles and brothers). This means that, on average, more than 133 women or girls are killed every day by someone in their own family."
     },
      // question10
      {
@@ -97,6 +97,6 @@ let questions = [
             { text: "1997", correct: true },
             { text: "1952", correct: false },     
         ],
-        explanation: "1979ão."
+        explanation: "In 1997 laws were amended to criminalize marital rape, incorporate gender-neutral language, and recognize the effect of psychological coercion. In 2016 German laws were rewritten to remove a previous requirement that a victim physically resist their assailants and be overcome by force."
     }
 ];
